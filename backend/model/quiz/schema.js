537f60d9bfb9eeb11d14b45f0ffbbe6ff9640320
tudor.sandu <tudor.sandu@webamboos.ro>
2018-11-25 00:45:42 +0200

    added right schema

--- backend/model/quiz/schema.js
+++ backend/model/quiz/schema.js
@@ -6,15 +6,15 @@ const quizSchema = new Schema({
   scored: { type: Boolean, default: false },
   time_limit: { type: Boolean, default: false },
   max_time_allowed: { type: Number },
-  rules: [
-    {
-      rule_type: { type: String, enum: ["question", "category"] },
-      id: { type: String },
-      no: { type: Number },
-      difficulty_level: { type: Number }
-    }
-  ],
-  eventId: { type: Schema.Types.ObjectId, ref: "events", required: true }
-});
+  rules: [{
+    rule_type: {type: String, enum: ['question', 'category']},
+    id: {type: String},
+    no: {type: Number},
+    difficulty_level: {type: [Number]}
+  }],
+  eventId: { type: Schema.Types.ObjectId,
+    ref: 'events',
+    required: true }
+})
 
 module.exports = quizSchema;
