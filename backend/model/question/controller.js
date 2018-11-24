12d4164c75bbd6a283bfe10c55dccf7aba52f0e2
tudor.sandu <tudor.sandu@webamboos.ro>
2018-11-24 15:38:22 +0200

    finished sort filter questions

--- backend/model/question/controller.js
+++ backend/model/question/controller.js
@@ -14,14 +14,19 @@ class QuestionController extends Controller {
     if (question.choices && question.choices.length !== 0 && question.right_answers && question.right_answers.length !== 0) {
       objToAdd.choices = question.choices
       objToAdd.right_answers = question.right_answers
+      if (objToAdd.right_answers.length > 1) {
+        objToAdd.multiple_answer = true
+      }
       objToAdd.free_text = false
     } else objToAdd.free_text = true
 
     if (question.score) {
       objToAdd.score = question.score
+      objToAdd.scored = true
     }
     if (question.time_constraint) {
       objToAdd.time_constraint = question.time_constraint
+      objToAdd.time_constrained = true
     }
     if (question.image) {
       objToAdd.image = question.image
@@ -62,11 +67,15 @@ class QuestionController extends Controller {
           if (question.Choices && question.Answers) {
             objToAdd.choices = question.Choices.split(';')
             objToAdd.right_answers = question.Answers.split(';')
+            if (objToAdd.right_answers.length > 1) {
+              objToAdd.multiple_answer = true
+            }
             objToAdd.free_text = false
           } else objToAdd.free_text = true
 
           if (question.Score) {
             objToAdd.score = question.Score
+            objToAdd.scored = true
           }
 
           if (question.Difficulty) {
@@ -75,9 +84,11 @@ class QuestionController extends Controller {
 
           if (question.Time) {
             objToAdd.time_constraint = question.Time
+            objToAdd.time_constrained = true
           }
           return this.facade.find({}).then((questions) => {
             let questionTexts = questions.map((question) => question.text)
+            if (questionTexts.length === 0) questionTexts = ['']
             const similarity = stringSimilarity.findBestMatch(question.Question, questionTexts)
             if (similarity.bestMatch.rating < 0.7) { return this.facade.create(objToAdd) } else return Promise.resolve()
           })
@@ -88,6 +99,26 @@ class QuestionController extends Controller {
   }
 
   findWithQuery (req, res, next) {
+    if (req.query.filter_by) {
+      switch (req.query.filter_by) {
+        case 'multiple_answer':
+          req.query.multiple_answer = true
+          break
+        case 'single_answer':
+          req.query.multiple_answer = true
+          break
+        case 'free_text':
+          req.query.free_text = true
+          break
+        case 'scored':
+          req.query.scored = true
+          break
+        case 'time_constrained':
+          req.query.time_constrained = true
+          break
+      }
+      delete req.query.filter_by
+    }
     if (req.query.sort_by) {
       req.sort = true
       req.sort_by = req.query.sort_by
@@ -98,7 +129,6 @@ class QuestionController extends Controller {
       const query = req.query.text
       req.query.text = {$regex: query, $options: 'i'}
     }
-    console.log(req.query)
     return this.find(req, res, next)
   }
 }
